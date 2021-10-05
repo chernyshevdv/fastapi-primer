ccd8d575c1e4d93b008358de0498fe5f74dc3cad
kreachure <chernyshevdv@gmail.com>
2021-10-05 14:31:11 +0300

    Stage 7. FastAPI + DB is working

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI, APIRouter, Query, HTTPException, Request
+from fastapi import FastAPI, APIRouter, Query, HTTPException, Request, Depends
 from fastapi.templating import Jinja2Templates
 from typing import Any, Optional
-
+from sqlalchemy.orm import Session
 from pathlib import Path
+
 from app.schemas import Recipe, RecipeCreate, RecipeSearchResults
 from app.recipe_data import RECIPES
+from app import deps
+from app import crud
 
 
 BASE_PATH = Path(__file__).resolve().parent
@@ -16,57 +19,53 @@ api_router = APIRouter()
 
 
 @api_router.get("/", status_code=200)
-def root(request: Request) -> dict:
+def root(request: Request, db: Session = Depends(deps.get_db)) -> dict:
     """ Root GET """
+    recipes = crud.recipe.get_multi(db=db, limit=10)
     return TEMPLATES.TemplateResponse(
         "index.html",
-        {"request": request, "recipes": RECIPES}
+        {"request": request, "recipes": recipes}
     )
 
 
 @api_router.get("/recipe/{recipe_id}", status_code=200, response_model=Recipe)
-def fetch_recipe(*, recipe_id: int) -> dict:
+def fetch_recipe(*, recipe_id: int, db: Session = Depends(deps.get_db)) -> Any:
     "Fetch a single recipe by ID"
-    result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    result = crud.recipe.get(db, recipe_id)
     if not result:
         raise HTTPException(
             status_code=404,
             detail=f"Recipe with ID {recipe_id} is not found"
         )
     
-    return result[0]
+    return result
 
 
 @api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
 def search_recipes(
     *,
     keyword: Optional[str]=Query(None, min_length=3, example="chicken"), 
-    max_results: Optional[int]=10) -> dict:
+    max_results: Optional[int]=10,
+    db: Session = Depends(deps.get_db),
+    ) -> dict:
     """
     Search for recipes based on label keyword
     """
+    recipes = crud.recipe.get_multi(db=db, limit=max_results)
     if not keyword:
-        return {"results": RECIPES[:max_results]}
+        return {"results": recipes}
     
-    results = filter(lambda recipe: keyword.lower() in recipe["label"].lower(), RECIPES)
+    results = filter(lambda recipe: keyword.lower() in recipe.label.lower(), recipes)
     return {"results": list(results)[:max_results]}
 
 
 @api_router.post("/recipe/", status_code=201, response_model=Recipe)
-def create_recipe(*, recipe_in: RecipeCreate) -> dict:
+def create_recipe(*, recipe_in: RecipeCreate, db: Session = Depends(deps.get_db)) -> dict:
     """
     Create a new recipe (in memory only)
     """
-    new_entry_id = len(RECIPES) + 1
-    recipe_entry = Recipe(
-        id=new_entry_id,
-        label=recipe_in.label,
-        source=recipe_in.source,
-        url=recipe_in.url,
-    )
-    RECIPES.append(recipe_entry.dict())
-    return recipe_entry
-
+    recipe = crud.recipe.create(db=db, obj_in=recipe_in)
+    return recipe
 
 app.include_router(api_router)
 
